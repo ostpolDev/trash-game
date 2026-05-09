781f38ea73bf313f1fd776f4a1b94b96fdb9f5e7
ostpolDev <alexander.hitzinger@gmail.com>
2026-05-09 20:32:08 +0200

    Add project files.

---
+++ OceanFactoryDesktop/Program.cs
@@ -0,0 +1,13 @@
+﻿using OceanFactory;
+
+namespace OceanFactoryDesktop;
+
+public class Program {
+
+    public static void Main() {
+        using var game = new OceanFactoryGame();
+        game.Run();
+    }
+
+}
+
