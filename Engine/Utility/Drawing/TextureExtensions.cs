c5b1dfc1fa2ad0e6d62b806d60127bf15c8d5a2b
ostpolDev <alexander.hitzinger@gmail.com>
2026-05-21 13:55:58 +0200

    Added mipmap param

--- Engine/Utility/Drawing/TextureExtensions.cs
+++ Engine/Utility/Drawing/TextureExtensions.cs
@@ -11,11 +11,11 @@ public static class TextureExtensions {
         target.SetData(0, destinationArea, colors, 0, colors.Length);
     }
 
-    public static Texture2D Slice(this Texture2D source, GraphicsDevice device, Rectangle sourceArea) {
+    public static Texture2D Slice(this Texture2D source, GraphicsDevice device, Rectangle sourceArea, bool mipmap = false) {
         Color[] colors = new Color[sourceArea.Width * sourceArea.Height];
         source.GetData(0, sourceArea, colors, 0, colors.Length);
 
-        Texture2D texture2D = new(device, sourceArea.Width, sourceArea.Height, false, SurfaceFormat.Color);
+        Texture2D texture2D = new(device, sourceArea.Width, sourceArea.Height, mipmap, SurfaceFormat.Color);
         texture2D.SetData(colors);
 
         return texture2D;
