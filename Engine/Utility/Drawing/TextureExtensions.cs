041b511145d230d829ddbca90ea41442a1584f41
ostpolDev <alexander.hitzinger@gmail.com>
2026-05-21 13:33:12 +0200

    Created TextureExtensions

---
+++ Engine/Utility/Drawing/TextureExtensions.cs
@@ -0,0 +1,24 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Engine.Utility.Drawing;
+
+public static class TextureExtensions {
+
+    public static void Blit(this Texture2D target, Texture2D source, Rectangle sourceArea, Rectangle destinationArea) {
+        Color[] colors = new Color[sourceArea.Width * sourceArea.Height];
+        source.GetData(0, sourceArea, colors, 0, colors.Length);
+        target.SetData(0, destinationArea, colors, 0, colors.Length);
+    }
+
+    public static Texture2D Slice(this Texture2D source, GraphicsDevice device, Rectangle sourceArea) {
+        Color[] colors = new Color[sourceArea.Width * sourceArea.Height];
+        source.GetData(0, sourceArea, colors, 0, colors.Length);
+
+        Texture2D texture2D = new(device, sourceArea.Width, sourceArea.Height, false, SurfaceFormat.Color);
+        texture2D.SetData(colors);
+
+        return texture2D;
+    }
+
+}
