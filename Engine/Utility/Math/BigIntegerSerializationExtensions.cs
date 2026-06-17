6b22a18cd9829242a55866fdc6ce272483398df4
ostpolDev <alexander.hitzinger@gmail.com>
2026-06-17 23:04:36 +0200

    Added SerializableDictionary extensions

--- Engine/Utility/Math/BigIntegerSerializationExtensions.cs
+++ Engine/Utility/Math/BigIntegerSerializationExtensions.cs
@@ -1,3 +1,4 @@
+using Engine.Serialization;
 using System.IO;
 using System.Numerics;
 
@@ -14,6 +15,17 @@ public static class BigIntegerSerializationExtensions {
     public static BigInteger ReadBigInteger(this BinaryReader reader) {
         int size = reader.ReadInt32();
         return new(reader.ReadBytes(size));
+    }
+
+    public static BigInteger GetBigInteger(this SerializableDictionary dictionary, string key) {
+        byte[] bytes = dictionary.GetBytes(key);
+        if (bytes == null) return default;
+
+        return new(bytes);
+    }
+
+    public static void Put(this SerializableDictionary dictionary, string key, BigInteger bigInteger) {
+        dictionary.Put(key, bigInteger.ToByteArray());
     }
 
 }
